195a4dba0b993e8ba099643efce4d0d21d88fc9c
RicardoCalvoP <a01028889@tec.mx>
2024-10-25 18:35:49 -0600

    act 2

--- Codes/Homeworks/A01028889_Act1.2.cpp
+++ Codes/Homeworks/A01028889_Act1.2.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h> // For INT_MAX
 
 using namespace std;
 
@@ -32,21 +33,75 @@ void Show(int list_numbers[], int size) // Complejidad O(n)
     cout << list_numbers[size] << "]" << endl
          << endl;
 }
-
-void Dynamic_Programming(int denominatios[], int change)
+void Show(int list_numbers[], int denominations[], int size) // Complejidad O(n)
 {
-    if (change == 0)
-        return;
-    cout << "change: " << change << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << list_numbers[i] << " coins of " << denominations[i] << endl;
+    }
 }
 
+// Best case complexity O(n),
+// or O(n*k) where n is the change needed and k is the num_of_domination
+void Dynamic_Programming(int denominations[], int num_of_domination, int change)
+{
+    if (change == 0)
+    {
+        cout << "No change needed." << endl;
+        return;
+    }
+
+    int dp[change + 1];       // Array to store the minimum number of coins for each amount
+    int coinUsed[change + 1]; // Array to store the last coin used for each amount
+
+    // Initialize dp and coinUsed arrays
+    for (int i = 0; i <= change; i++)
+    {
+        dp[i] = INT_MAX - 1; // Avoid overflow
+        coinUsed[i] = -1;
+    }
+    dp[0] = 0; // No coins needed for amount 0
+
+    // Fill dp array
+    for (int i = 1; i <= change; i++)
+    {
+        for (int j = 0; j < num_of_domination; j++)
+        {
+            if (i >= denominations[j] && dp[i - denominations[j]] != INT_MAX - 1)
+            {
+                if (dp[i] > dp[i - denominations[j]] + 1)
+                {
+                    dp[i] = dp[i - denominations[j]] + 1;
+                    coinUsed[i] = j;
+                }
+            }
+        }
+    }
+
+    int amount = change;
+    int coin_count[num_of_domination] = {0}; // Array to store the count of each coin used
+
+    // Reconstruct the solution
+    while (amount > 0)
+    {
+        int coin_index = coinUsed[amount];
+        coin_count[coin_index]++;
+        amount -= denominations[coin_index];
+    }
+
+    // Print the number of coins of each denomination used
+    Show(coin_count, denominations, num_of_domination);
+}
+
+// Complexity O(n)
 void Greedy(int denominations[], int change)
 {
     int coins[change];
     int num_transaction = 0;
     while (change > 0)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 6; i++)
         {
             if (change >= denominations[i])
             {
@@ -75,12 +130,23 @@ void Solution(int denominations[], int num_cases)
     for (int current_case = 0; current_case < num_cases; current_case++)
     {
         cout << "Case " << current_case + 1 << " change " << to_evaluate_num_list[current_case] << ":" << endl;
+<<<<<<< HEAD
         cout << "Dinamic Programming Solution:" << endl;
         // Dynamic_Programming(denominations, to_evaluate_num_list[current_case]);
         cout
             << "Greedy Solution: " << endl;
 
         Greedy(denominations, to_evaluate_num_list[current_case]);
+=======
+        cout << "Dynamic Programming Solution:" << endl;
+        // Dynamic Programming will send number of coins used to get to solution
+        Dynamic_Programming(denomitations, 6, to_evaluate_num_list[current_case]);
+        cout
+            << "Greedy Solution: " << endl;
+
+        // ---+
+        Greedy(denomitations, to_evaluate_num_list[current_case]);
+>>>>>>> bb2ef9438216b4f1cce21ea841fe8d5e48a6b882
     }
 }
 
@@ -89,8 +155,8 @@ int main()
     system("cls"); // Borrar contenido de terminal
     srand((unsigned)time(NULL));
 
-    int denominations[] = {5, 2, 1}; // value of coins
-    int num_cases = 4;               // number of cases to test
+    int denominations[] = {50, 25, 20, 10, 5, 1}; // value of coins
+    int num_cases = 4;                            // number of cases to test
 
     Solution(denominations, num_cases);
     return 0;
