e01f5db2d1d2415f5df65c9b3ed9e7beee80365f
RicardoCalvoP <a01028889@tec.mx>
2024-11-07 20:09:26 -0600

    hw 3 added floyd warshall

--- Codes/Homeworks/a01028889_Act.3.2.cpp
+++ Codes/Homeworks/a01028889_Act.3.2.cpp
@@ -3,26 +3,137 @@
 #include <fstream>
 #include <stack>
 #include <queue>
+#include <limits>
 
 using namespace std;
+const int INF = numeric_limits<int>::max();
 
-void read_Adjacency_List(vector<vector<int>> adjacency_list)
+/*
+=========================================================================
+Floyd Warshall
+=========================================================================
+*/
+void read_Adjacency_Matrix(vector<vector<int>> adjacency_matrix)
 {
-  for (int i = 0; i < adjacency_list.size(); i++)
+  for (int i = 0; i < adjacency_matrix.size(); i++)
   {
     cout << "Node " << i + 1 << ": ";
-    for (int j = 0; j < adjacency_list[i].size(); j++)
+    for (int j = 0; j < adjacency_matrix[i].size(); j++)
     {
-      cout << adjacency_list[i][j] + 1 << " ";
+      if (adjacency_matrix[i][j] < 0)
+      {
+
+        cout << adjacency_matrix[i][j] << " ";
+      }
+      else if (adjacency_matrix[i][j] > 9)
+      {
+        cout << " " << adjacency_matrix[i][j];
+      }
+      else
+      {
+        cout << " " << adjacency_matrix[i][j] << " ";
+      }
     }
     cout << endl;
   }
 }
 
-vector<vector<int>> readGraph(string file)
+vector<vector<int>> get_Adjacency_Matrix(string file)
 {
   int nodes, edges;
-  vector<vector<int>> adjacency_list;
+  vector<vector<int>> adjacency_matrix;
+  ifstream myFile(file);
+  if (myFile.is_open())
+  {
+    myFile >> nodes >> edges;
+
+    adjacency_matrix.resize(nodes, vector<int>(nodes, -1));
+
+    // set in 0 diagonal line in matrix
+    for (int i = 0; i < nodes; i++)
+    {
+      adjacency_matrix[i][i] = 0;
+    }
+
+    for (int i = 0; i < edges; i++)
+    {
+      // u = nodo origen
+      // v = nodo destino
+      // c = costo de la arista
+      int u, v, c;
+      myFile >> u >> v >> c;
+
+      // Guardar el costo en la posición correspondiente
+      adjacency_matrix[u - 1][v - 1] = c;
+    }
+  }
+  else
+  {
+    cout << "Could not open file" << endl;
+  }
+  return adjacency_matrix;
+}
+
+vector<vector<int>> floydWarshall(vector<vector<int>> matrix)
+{
+  int n = matrix.size();
+  vector<vector<int>> dist = matrix; // Copia de la matriz de entrada para calcular las distancias
+
+  // Reemplazar -1 por INF (excepto en la diagonal principal)
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      if (dist[i][j] == -1 && i != j) // No hay conexión directa
+      {
+        dist[i][j] = INF;
+      }
+    }
+  }
+
+  // Aplicamos Floyd-Warshall
+  for (int k = 0; k < n; ++k) // Nodo intermedio
+  {
+    for (int i = 0; i < n; ++i) // Nodo origen
+    {
+      for (int j = 0; j < n; ++j) // Nodo destino
+      {
+        // Si hay un camino válido a través de k, actualizamos la distancia
+        if (dist[i][k] != INF && dist[k][j] != INF)
+        {
+          dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+        }
+      }
+    }
+  }
+
+  return dist;
+}
+void read_Adjacency_List(vector<vector<vector<int>>> adjacency_list)
+{
+  for (int i = 0; i < adjacency_list.size(); i++)
+  {
+    cout << "Node " << i + 1 << ": ";
+    for (int j = 0; j < adjacency_list[i].size(); ++j)
+    {
+      if (j != adjacency_list[i].size() - 1)
+      {
+        cout << "[" << adjacency_list[i][j][0] << " , " << adjacency_list[i][j][1] << "] - ";
+      }
+      else
+      {
+        cout << "[" << adjacency_list[i][j][0] << " , " << adjacency_list[i][j][1] << "]";
+      }
+    }
+
+    cout << endl;
+  }
+}
+
+vector<vector<vector<int>>> get_Adjacency_List(string file)
+{
+  int nodes, edges;
+  vector<vector<vector<int>>> adjacency_list;
   ifstream myFile(file);
   if (myFile.is_open())
   {
@@ -38,9 +149,8 @@ vector<vector<int>> readGraph(string file)
       // w = weight
       int u, v, w;
       myFile >> u >> v >> w;
-      adjacency_list[u - 1].push_back(w);
-      cout << "adjacency_list version " << i << endl;
-      read_Adjacency_List(adjacency_list);
+      vector<int> vw = {v, w};
+      adjacency_list[u - 1].push_back(vw);
     }
   }
   else
@@ -50,68 +160,24 @@ vector<vector<int>> readGraph(string file)
   return adjacency_list;
 }
 
-void DFS(vector<vector<int>> graph, int start)
-{
-  stack<int> my_stack;
-  int current;
-  vector<bool> visited(graph.size(), false);
-  my_stack.push(start);
-  while (!my_stack.empty())
-  {
-    current = my_stack.top();
-    my_stack.pop();
-    if (!visited[current])
-    {
-      cout << current << " ";
-      visited[current] = true;
-      for (int i = 0; i < graph[current].size(); i++)
-      {
-        if (!visited[graph[current][i]])
-        {
-          my_stack.push(graph[current][i]);
-        }
-      }
-    }
-  }
-}
-
-void BFS(vector<vector<int>> graph, int start)
-{
-  queue<int> my_stack;
-  int current;
-  vector<bool> visited(graph.size(), false);
-  my_stack.push(start);
-  while (!my_stack.empty())
-  {
-    current = my_stack.front();
-    my_stack.pop();
-    if (!visited[current])
-    {
-      cout << current << " ";
-      visited[current] = true;
-      for (int i = 0; i < graph[current].size(); i++)
-      {
-        if (!visited[graph[current][i]])
-        {
-          my_stack.push(graph[current][i]);
-        }
-      }
-    }
-  }
-}
-
 int main()
 {
   system("cls"); // Borrar contenido de terminal
 
-  vector<vector<int>> adjacency_list = readGraph("../../grafo.txt");
+  vector<vector<int>> adjacency_matrix = get_Adjacency_Matrix("../../grafo.txt");
+  cout << "Adjacency Matrix" << endl;
+  read_Adjacency_Matrix(adjacency_matrix);
+
+  vector<vector<vector<int>>> adjacency_list = get_Adjacency_List("../../grafo.txt");
+  cout << endl
+       << "Adjacency List:" << endl;
   read_Adjacency_List(adjacency_list);
 
-  cout << "DFS" << endl;
-  DFS(adjacency_list, 0);
-  cout << endl;
-  cout << "BFS" << endl;
-  BFS(adjacency_list, 0);
+  vector<vector<int>> Floyd_matrix = floydWarshall(adjacency_matrix);
+  cout << endl
+       << "Adjacency Matrix with floyd" << endl;
+  read_Adjacency_Matrix(Floyd_matrix);
+
   cout << endl;
   return 0;
 }
